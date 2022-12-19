c70d53a7fdc1d7360b26c4a849934e0963f924cb
Riley Ball <r.ball@uqconnect.edu.au>
2022-12-19 14:41:20 +1000

    added eq, hash and deepcopy function to State object

--- state.py
+++ state.py
@@ -4,3 +4,12 @@ class State():
     def __init__(self, environment, board):
         self.board = board
         self.environment = environment
+
+    def __eq__(self, other):
+        return self.board == other.board
+
+    def __hash(self):
+        return hash(str(self.board))
+
+    def deepcopy(self):
+        return State(self.environment, [row[:] for row in self.board])
